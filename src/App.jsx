495f28c419eed3a7096bac3a04a73ed6d24ea2d2
iJulixn <126498282+iJulixn@users.noreply.github.com>
2026-04-20 22:58:07 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{padding:20}}>
+      <h1>JN Garage Detail 🚗</h1>
+      <p>App lista para usar</p>
+      <p>Login demo: julian / 1234</p>
+    </div>
+  )
+}
